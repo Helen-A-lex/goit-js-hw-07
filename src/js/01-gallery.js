b75af29ca46d1d68e2bd30ef5d067628050e453b
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-03-28 17:19:50 +0200

    add task 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,59 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+  })
+  .join("");
+const galleryContainer = document.querySelector(".gallery");
+console.log(galleryContainer);
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  const image = evt.target;
+  if (!image.classList.contains("gallery__image")) {
+    return;
+  }
+  console.log(image);
+
+  const originalImageUrl = image.dataset.source;
+  const currentImage = galleryItems.find(
+    ({ original }) => original === originalImageUrl
+  );
+
+  function onEscKeyPress(evt) {
+    console.log(evt.code);
+    const isKeyCodeEscape = evt.code === "Escape";
+    if (isKeyCodeEscape) {
+      instance.close();
+    }
+  }
+
+  const instance = basicLightbox.create(
+    `<div class="modal">
+    <img src="${currentImage.original}" alt="${currentImage.description}" width="800" height="600" />
+  </div>`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+
+  instance.show();
+}
